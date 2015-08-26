b3121705745ea0e5bf89e18ae7a2cca6bdce2cc9
Eric Dallo <ericdallo06@hotmail.com>
2015-08-26 17:30:44 -0300

    Adiciona validacao quando não é encontrado um cubo

--- app/src/main/java/com/cubomania/Fragment/MainActivityFragment.java
+++ app/src/main/java/com/cubomania/Fragment/MainActivityFragment.java
@@ -3,13 +3,11 @@ package com.cubomania.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,10 +17,14 @@ import com.cubomania.Image.ImageLoader;
 import com.cubomania.R;
 import com.cubomania.Service.ServiceCubes;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class MainActivityFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
+    public static final String SELECT_SIZE = "Selecione um tamanho";
+    public static final String SELECT_TYPE = "Selecione um tipo";
+    private static final String SELECT_DIFICULTY = "Selecione uma dificuldade";
     private ServiceCubes serviceCubes;
     private TextView tvPrice, tvName, tvSize, tvType, tvDificulty;
     private ImageView ivCube;
@@ -31,6 +33,7 @@ public class MainActivityFragment extends Fragment implements AdapterView.OnItem
     private Cube currentCube;
     private int currentPosition = 0;
     private ImageLoader imageLoader;
+    private Spinner spinnerSize,spinnerType,spinnerDificulty;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
@@ -39,9 +42,9 @@ public class MainActivityFragment extends Fragment implements AdapterView.OnItem
         serviceCubes = new ServiceCubes(this);
         imageLoader = new ImageLoader(this.getContext());
 
-        createSpinner(view,R.id.cube_size,R.array.cube_size);
-        createSpinner(view,R.id.cube_type,R.array.cube_type);
-        createSpinner(view,R.id.cube_dificulty,R.array.cube_dificulty);
+        spinnerSize = createSpinner(view,R.id.cube_size,R.array.cube_size);
+        spinnerType = createSpinner(view, R.id.cube_type, R.array.cube_type);
+        spinnerDificulty = createSpinner(view,R.id.cube_dificulty,R.array.cube_dificulty);
 
         tvName = (TextView) view.findViewById(R.id.cube_title);
         tvSize = (TextView) view.findViewById(R.id.res_size);
@@ -59,19 +62,20 @@ public class MainActivityFragment extends Fragment implements AdapterView.OnItem
         checkIfEnableNext();
         checkIfEnablePrevious();
 
-        btNext.setOnClickListener((View v) -> nextCube());
-        btPrevious.setOnClickListener((View v) -> previousCube());
-        btSearch.setOnClickListener((View v) -> serviceCubes.getAll() );
+        btNext.setOnClickListener( (View v) -> nextCube() );
+        btPrevious.setOnClickListener( (View v) -> previousCube() );
+        btSearch.setOnClickListener( (View v) -> serviceCubes.getAll() );
 
         return view;
     }
 
-    private void createSpinner(View view, int spinnerId, int cubeList) {
+    private Spinner createSpinner(View view, int spinnerId, int cubeList) {
         Spinner spinner = (Spinner) view.findViewById(spinnerId);
         ArrayAdapter<CharSequence> items = ArrayAdapter.createFromResource(view.getContext(),cubeList,android.R.layout.simple_spinner_item);
         items.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(items);
+        return spinner;
     }
 
 
@@ -85,6 +89,7 @@ public class MainActivityFragment extends Fragment implements AdapterView.OnItem
     }
 
     public void updateCubes(List<Cube> cubeList) {
+        sortListBySpinners(cubeList);
         this.cubeList = cubeList;
         this.serviceCubes = new ServiceCubes(this);
         currentPosition = 0;
@@ -97,10 +102,41 @@ public class MainActivityFragment extends Fragment implements AdapterView.OnItem
             tvDificulty.setText(currentCube.getDificuldade());
             tvPrice.setText(currentCube.getPreco()+ "");
             imageLoader.displayImage(currentCube.getImagem(), ivCube);
+        }else{
+            currentCube = null;
+            tvName.setText(R.string.no_cubes);
+            tvSize.setText("");
+            tvType.setText("");
+            tvDificulty.setText("");
+            tvPrice.setText("");
+            ivCube.setImageResource(R.drawable.black_cube_init);
         }
 
         checkIfEnableNext();
         checkIfEnablePrevious();
+    }
+
+    private void sortListBySpinners(List<Cube> cubeList) {
+        String size = spinnerSize.getSelectedItem().toString();
+        String type = spinnerType.getSelectedItem().toString();
+        String dificulty = spinnerDificulty.getSelectedItem().toString();
+
+        removeFromList(size,cubeList,SELECT_SIZE);
+        removeFromList(type,cubeList,SELECT_TYPE);
+        removeFromList(dificulty,cubeList,SELECT_DIFICULTY);
+    }
+
+    private void removeFromList(String filter, List<Cube> listCubes, String select){
+        if (!filter.equals(select)){
+            for (Iterator<Cube> it = listCubes.iterator(); it.hasNext();) {
+                Cube cube = it.next();
+                if ((!cube.getTamanho().equals(filter) && select.equals(SELECT_SIZE)) ||
+                        (!cube.getTipo().equals(filter) && select.equals(SELECT_TYPE) ) ||
+                        (!cube.getDificuldade().equals(filter) && select.equals(SELECT_DIFICULTY))){
+                    it.remove();
+                }
+            }
+        }
     }
 
     private void nextCube() {
--- app/src/main/java/com/cubomania/MainActivity.java
+++ app/src/main/java/com/cubomania/MainActivity.java
@@ -11,14 +11,12 @@ import com.cubomania.Image.MemoryCache;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
